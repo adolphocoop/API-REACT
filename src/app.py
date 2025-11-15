15e329e21268fdb7cde1a7454dac6033bfab1408
Adolfo <adolphovt12@gmail.com>
2025-11-14 20:59:24 -0600

    Primer Commit

---
+++ src/app.py
@@ -0,0 +1,73 @@
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo, ObjectId
+from flask_cors import CORS
+
+#requerimos pymongo
+
+
+app = Flask(__name__)
+#Configuracion de mongodb
+app.config['MONGO_URI']='mongodb://localhost/pythonreactdb'
+
+mongo =PyMongo(app)
+
+db = mongo.db.users
+
+
+
+@app.route('/users', methods=['POST'])
+def createUser():
+  print(request.json)
+  id = db.insert({
+    'name': request.json['name'],
+    'email': request.json['email'],
+    'password': request.json['password']
+  })
+  #print(str(ObjectId(id)))
+  #vamos a retornar el id atraves jsonify
+
+  return jsonify(str(ObjectId(id)))
+
+@app.route('/users', methods=['GET'])
+def getUsers():
+    #Creamos una lista users
+    users=[]
+    #Por cada documento en la respuesta db.find
+    for doc in db.find():
+        #Vamos añadir los objetos a la lista 
+        users.append({
+            #La conversion del id a un string 
+            '_id': str(ObjectId(doc['_id'])),
+            'name': doc['name'],
+            'email': doc['email'],
+            'password': doc['password']
+        })
+
+    return jsonify(users)
+
+@app.route('/user/<id>', methods=['GET'])
+def getUser(id):
+    #vamos abuscar el dato y lo guardamos en la variable user
+    user = db.find_one({'_id': ObjectId(id)})
+
+    return jsonify({
+        #Se va a convertir el id a string
+        '_id': str(ObjectId(user['_id'])),
+        'name': user['name'],
+            'email': user['email'],
+            'password': user['password']
+            #Pendiente min 25:41
+
+    })
+
+@app.route('/users/<id>', methods=['GET'])
+def deleteUser():
+    return 'Hola bb'
+
+@app.route('/users/<id>', methods=['PUT'])
+def updateUser():
+    return 'Hola bb'
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
